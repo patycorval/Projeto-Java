bd92901214c3582ab4f8508ddc214d3fbb51daa7
Diego Guaglianone <diegofguaglianone@gmail.com>
2025-06-10 17:42:39 -0300

    ajuste para puxar a lista correta

--- src/main/java/com/example/reservaSala/repository/SalaRepository.java
+++ src/main/java/com/example/reservaSala/repository/SalaRepository.java
@@ -1,6 +1,7 @@
 package com.example.reservaSala.repository;
 
 import com.example.reservaSala.model.Sala;
+import com.example.reservaSala.model.enums.Recurso;
 import com.example.reservaSala.model.enums.TipoSala;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,5 +17,5 @@ public interface SalaRepository extends JpaRepository<Sala, Long> {
 
     List<Sala> findByTipo(TipoSala tipo); // Filtrar por tipo de sala
 
-    List<Sala> findByRecursosContaining(String recurso); // Buscar salas com recurso específico (ex: "PROJETOR")
+    List<Sala> findByRecursosContaining(Recurso recurso); // Buscar salas com recurso específico (ex: "PROJETOR")
 }
