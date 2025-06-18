63cba1409396828e43f1b40dc54eee500839fb53
Diego Guaglianone <diegofguaglianone@gmail.com>
2025-06-17 22:05:12 -0300

    so para rodar no meu

--- src/main/java/com/example/reservaSala/controller/SalaController.java
+++ src/main/java/com/example/reservaSala/controller/SalaController.java
@@ -1,14 +1,9 @@
 package com.example.reservaSala.controller;
 
-import com.example.reservaSala.model.Sala;
 import com.example.reservaSala.service.SalaService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/salas")
