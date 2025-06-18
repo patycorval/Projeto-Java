23bf155d581bec090b4dcd861ad8bb03f57a4c49
Patrícia Corval <paticorval@gmail.com>
2025-06-18 14:24:36 -0300

    Ajustando a quantidade de computadores

--- src/main/java/com/example/reservaSala/config/InicializadorSalas.java
+++ src/main/java/com/example/reservaSala/config/InicializadorSalas.java
@@ -12,41 +12,41 @@ import java.util.List;
 @Component
 public class InicializadorSalas implements CommandLineRunner {
 
-    private final SalaService salaService;
+        private final SalaService salaService;
 
-    public InicializadorSalas(SalaService salaService) {
-        this.salaService = salaService;
-    }
+        public InicializadorSalas(SalaService salaService) {
+                this.salaService = salaService;
+        }
 
-    @Override
-    public void run(String... args) {
-        salaService.apagarTodasSalas();
+        @Override
+        public void run(String... args) {
+                salaService.apagarTodasSalas();
 
-        List<Sala> salas = List.of(
-                new Sala("3.01", 50, "3º andar", TipoSala.LABORATORIO, true,
-                        List.of(Recurso.TELEVISOR)),
-                new Sala("3.02", 40, "3º andar", TipoSala.LABORATORIO, true,
-                        List.of(Recurso.TELEVISOR)),
-                new Sala("3.03", 45, "3º andar", TipoSala.LABORATORIO, true,
-                        List.of(Recurso.PROJETOR)),
-                new Sala("3.04", 60, "3º andar", TipoSala.LABORATORIO, true,
-                        List.of(Recurso.TELEVISOR)),
-                new Sala("3.05", 60, "3º andar", TipoSala.LABORATORIO, true,
-                        List.of(Recurso.TELEVISOR)),
+                List<Sala> salas = List.of(
+                                new Sala("3.01", 50, "3º andar", TipoSala.LABORATORIO, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.TELEVISOR), 20),
+                                new Sala("3.02", 40, "3º andar", TipoSala.LABORATORIO, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.TELEVISOR), 15),
+                                new Sala("3.03", 45, "3º andar", TipoSala.LABORATORIO, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.PROJETOR), 24),
+                                new Sala("3.04", 60, "3º andar", TipoSala.LABORATORIO, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.TELEVISOR), 25),
+                                new Sala("3.05", 60, "3º andar", TipoSala.LABORATORIO, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.PROJETOR), 18),
 
-                // 5º andar
-                new Sala("501", 50, "5º andar", TipoSala.SALA_AULA, true,
-                        List.of(Recurso.TELEVISOR)),
-                new Sala("502", 40, "5º andar", TipoSala.SALA_AULA, true,
-                        List.of(Recurso.TELEVISOR)),
-                new Sala("503", 45, "5º andar", TipoSala.SALA_AULA, true,
-                        List.of(Recurso.PROJETOR)),
-                new Sala("504", 60, "5º andar", TipoSala.SALA_AULA, true,
-                        List.of(Recurso.TELEVISOR)),
-                new Sala("505", 60, "5º andar", TipoSala.SALA_AULA, true,
-                        List.of(Recurso.TELEVISOR)));
+                                // 5º andar
+                                new Sala("501", 50, "5º andar", TipoSala.SALA_AULA, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.PROJETOR), 0),
+                                new Sala("502", 40, "5º andar", TipoSala.SALA_AULA, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.TELEVISOR), 0),
+                                new Sala("503", 45, "5º andar", TipoSala.SALA_AULA, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.PROJETOR), 0),
+                                new Sala("504", 60, "5º andar", TipoSala.SALA_AULA, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.TELEVISOR), 0),
+                                new Sala("505", 60, "5º andar", TipoSala.SALA_AULA, true,
+                                                List.of(Recurso.COMPUTADORES, Recurso.TELEVISOR), 0));
 
-        salaService.salvarTodas(salas);
-        System.out.println("✅ Salas pré-definidas carregadas com sucesso.");
-    }
+                salaService.salvarTodas(salas);
+                System.out.println("✅ Salas pré-definidas carregadas com sucesso.");
+        }
 }
